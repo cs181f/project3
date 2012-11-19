61de94137eeca51486b4dea57122b807243cbbf4
Jesse Pollak <jpollak92@gmail.com>
2012-11-19 00:08:20 -0800

    adds design for BuildQueue

---
+++ rosie/rosie/app.py
@@ -0,0 +1,13 @@
+# library requirements for server
+from flask import (
+    Flask,
+    render_template,
+    session,
+    flash,
+    redirect,
+    request,
+    url_for
+)
+
+# Linnea needs to add requirements for her mongoDB interaction
+
