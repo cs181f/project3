3cb8a89604837f8865a731a11cd52ee2a22c7354
Brennen <brennen@clef.io>
2012-12-02 23:23:58 -0800

    Updates api and api_test in line with review

--- rosie/rosie/tests/api_test.py
+++ rosie/rosie/tests/api_test.py
@@ -4,12 +4,24 @@ API responds correctly to calls of all endpoints.
 BLACKBOX TESTING:
 
     def test_api_builds():
+    def test_api_builds_bad():
+    def test_api_builds_loop():
     def test_api_pings():
     def test_api_build_status():
+    def test_api_build_status_wrong_id():
     def test_api_build_statuses():
-    def test_api_accepts settings changes():
+    def test_api_accepts_settings_changes():
+    def test_api_denies_bad_settings():
     def test_api_blame_list():
     def test_api_rebuilds():
+
+WHITEBOX TESTING:
+
+    Since the API is reaching deeper into the server to do any excecution
+    and is not doing any of the lifting itself, there is no logic to white
+    box test. We need to be sure that the appropriate piece of data is returned
+    or that the correct internal method is triggered, but do not need to examine
+    them here.        
 """
 
 
@@ -21,37 +33,52 @@ from views import (
 class APITest(unittest.TestCase):
     """Test cases for API"""
     @classmethod
-    def setUpClass(self):
-        """ Nothing needs to be set up """
+    def set_up_class(self):
+        """ Create clean instance of DB according to test_settings file"""
 
-    def setUp(self):
-        """ Create a build object to test with"""
+    def set_up(self):
+        """ Create build objects to test with"""
 
-    def tearDown(self):
-        """ Nothing needs to be torn down """
+    def tear_down(self):
+        """ Remove all objects from DB and remove it """
 
     def test_api_builds():
-        """ Verifies that build is run correctly. """
+        """ Verifies that a good build builds without error """
+
+    def test_api_builds_bad():
+        """ Verifies that a bad build returns an error and posts the error to 
+        GitHub """
+
+    def test_api_builds_loop():
+        """ Verifies that a bad build with an infinite loop times out after the
+        hang time in test_settings """
 
     def test_api_pings():
-        """ Verifies that correct status is returned. """
+        """ Verifies that it returns true when building and false when free """
 
     def test_api_build_status():
         """ Verifies that when a build is running, returns the status of that
         build and otherwise returns clear."""
 
-    def test_api_build_statuses():
-        """ Verifies that all build statuses are returned correctly. """
+    def test_api_build_status_wrong_id():
+        """ Verifies that an error is returned if an invalid ID is passed to 
+        the build_status endpoint """
 
-    def test_api_accepts settings changes():
-        """ Verifies that settings changed pushed are reflected in the
-        config file """
+    def test_api_build_statuses():
+        """ Verifies that build statuses returned match initial set."""
+
+    def test_api_accepts_settings_changes():
+        """ Verifies that test_settings file is changed"""
+
+    def test_api_denies_bad_settings():
+        """ Verifies that test_settings file is not changed if settings are
+        malformed or incorrect """
 
     def test_api_blame_list():
-        """ Verifies that a properly ordered list is returned """
+        """ Verifies that the data returned is the same as data stored"""
 
     def test_api_rebuilds():
-        """ Verifies that a build is properly rerun with new tests """
-    
+        """ Verifies that after adding a failing test, build that passed build()
+        now fails."""
 
 
--- rosie/rosie/views/api.py
+++ rosie/rosie/views/api.py
@@ -6,47 +6,58 @@ GitHub's Webhooks, the web frontend, and the command line interface.
 Each API route is either returning accessible information or saving it and
 triggering events, so the complexity of each method is fairly low.
 """
-from flask import jsonify
+from flask import jsonify #translates a dictionary into JSON 
+                          #(http://flask.pocoo.org/docs/api/)
+from build import Build
 
 ###GitHub Webhook###
 
-@api.route('/build')
+@api.route('/build', methods=['POST']) 
+                     #The @api.route decorator is a flask function that 
+                     #triggers the method when that url is visited, with
+                     #access given to the paramaters passed in the URL.
+                     # http://flask.pocoo.org/docs/api/#api
 def build():
-    #parses Github Webhooks data into builds
-    #creates Build object and stores it in the database
+    #parses Github Webhooks data into JSON of bild
+    #creates Build object and stores it in the database by calling the public
+    #fill method
 
 ###HTML Endpoints###
 
-@api.route('/ping')
+@api.route('/ping', methods=['POST'])
 def ping():
     #checks whether the worker is processing a build or free
     #returns jsonify(status of server)
 
-@api.route('/builds/<build_id>')
+@api.route('/builds/<build_id>', methods=['POST'])
 def get_build():
     #looks up the build by its id in the database
+    #the variable build_id is passed in the url 
     #looks at the status of the build
     #returns jsonify(status of build)
 
-@api.route('/builds')
+@api.route('/builds', methods=['POST'])
 def get_builds():
-    #looks at the status of all builds in the db
-    #returns jsonify(status of all builds)
+    #gets all builds in DB using .find() method
+    #returns data for all builds
 
-@api.route('/settings')
+@api.route('/settings', methods=['POST'])
 def settings():
     #accepts settings changes 
     #modifies config file with appropriate changes
 
-@api.route('/blame')
+@api.route('/check_settings', methods=['GET'])
+def get_settings():
+    #returns the contents of the settings file
+    #this is to populate the web interface and
+    #to provide information at the command line
+
+@api.route('/blame', methods=['POST'])
 def blame():
-    #in the database, there will be an object of broken commit ranks
-    #when a build breaks, the worker will increment the counter of that
-    #commit's author. 
     #This endpoint returns that list sorted with the highest number of broken
     #commits first.
 
-@api.route('/builds/new')
+@api.route('/builds/new', methods=['POST'])
 def rebuild():
     #takes build ID as parameter
     #looks up a build by that ID
